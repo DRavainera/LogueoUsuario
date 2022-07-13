ad88d88071317f1051afbc48e08438fd27f26dc5
Daniel <Daniel@DESKTOP-S7S6NPB>
2022-07-13 11:46:15 -0300

    Agregar archivos de proyecto.

---
+++ LogueoUsuario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LogueoUsuario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
